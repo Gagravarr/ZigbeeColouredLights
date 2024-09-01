2479ad7b9e17d7f095c71c145d77927a9438e5a3
Nick Burch <nick@gagravarr.org>
2024-09-01 18:45:48 +0100

    State changes via an enum

--- control.py
+++ control.py
@@ -4,6 +4,7 @@
 # Note that this would probably be easier with HomeAssistant...
 
 import signal, sys
+from enum import Enum
 from helpers import *
 
 # What lights to control
@@ -11,7 +12,7 @@ lights = ["Floor0/Dining/LightLeft","Floor0/Dining/LightRight"]
 # What button to watch
 button = "Floor0/Dining/SwitchLight"
 # Should we report what we're doing?
-set_verbose(False)
+verbose = set_verbose(True)
 # How long to listen for, unless specified as an argument
 run_minutes = 60
 
@@ -28,16 +29,56 @@ signal.signal(signal.SIGINT, make_shutdown_signal_handler(lights))
 # Begin with all lights off
 all_lights_off(lights)
 
+# The current state
+LightState = Enum("LightState", ["Off","White","Colour","Changing"])
+state = LightState.Off
+
+# Moves to the next state, with wraparound to Off
+def next_state():
+  global state
+  ns = state.value + 1
+  if ns in LightState:
+    state = LightState(ns)
+  else:
+    state = LightState.Off
+
 # Handles button presses
-def todo_button(msg):
-  print("Got one!")
-  print(msg)
+def button_pressed(msg):
+  global state
+
+  # Single or Double press?
+  double = msg.get("action",None) == "double"
+
+  # If it's off, anything means turn on
+  if state == LightState.Off:
+    if verbose:
+      print("Turning the lights on")
+    all_lights_on(lights)
+    next_state()
+    return
+
+  # If it's double, move to the next state
+  if double:
+    next_state()
+
+    if state == LightState.Off:
+      if verbose:
+        print("Turning the lights off")
+      state = LightState.Off.value
+      all_lights_off(lights)
+      return
+    if verbose:
+      print("Lights state changed to %s" % state.name)
+
+  # Change to the next setting of the current/new state
+  # TODO
 
 # Listen to the button
-receive(button, todo_button)
+receive(button, button_pressed)
 print("Now waiting...")
 
 
 # TODO Check the arguments to see how long to listen for
 client.loop_start()
 time.sleep(run_minutes * 60)
+# TODO support changing
