fe5c0a6ac4c177c43129f16ce29da1e06f968ed6
Nick Burch <nick@gagravarr.org>
2024-09-01 17:27:55 +0100

    Helper to turn the lights off directly

---
+++ off.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Ensures that the RGB-CCT lights have turned off, via Zigbee2MQTT
+
+import signal
+from helpers import *
+
+# What lights to control
+lights = ["Floor0/Dining/LightLeft","Floor0/Dining/LightRight"]
+# Should we report what we're doing?
+set_verbose(False)
+
+# MQTT details
+mqtt_server = "127.0.0.1"
+mqtt_port = 1883
+
+# Connect to the MQTT server
+connect(mqtt_server, mqtt_port)
+
+# Make sure the lights are on
+all_lights_off(lights)
+
+# Wait for that to go through before disconnecting
+time.sleep(2)
